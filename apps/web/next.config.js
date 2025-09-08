a77f21e845eb86a1556aa3ab49b00ce956cf3338
Tapan Upadhyay <tapanupa4321@gmail.com>
2025-09-08 20:53:00 +0530

    web: add TS deps and fix next.config.js (remove experimental.appDir)

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: { appDir: true }
+  // No experimental.appDir needed on Next 13+ App Router
 };
+
 module.exports = nextConfig;
